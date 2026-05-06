67cc4b7af362b449e27c45334e680ed9aba5b7d3
Izarot <sharananandanaraizarot@gmail.com>
2026-05-06 15:19:52 +0530

    Update app.js

--- ai/app.js
+++ ai/app.js
@@ -1,12 +1,44 @@
-import { Memory } from "./brain/memory.js";
-import { Mood } from "./brain/mood.js";
-import { Profile } from "./brain/profile.js";
-import { setupInput } from "./interface/input.js";
+// /ai/app.js
 
-const memory = new Memory();
-const mood = new Mood();
-const profile = new Profile();
+import { Memory }
+from "./brain/memory.js";
 
-window.onload = () => {
-  setupInput(memory, mood, profile);
-};
+import { Mood }
+from "./brain/mood.js";
+
+import { Profile }
+from "./brain/profile.js";
+
+import { setupInput }
+from "./interface/input.js";
+
+
+// CORE STATE
+
+const memory =
+  new Memory();
+
+const mood =
+  new Mood();
+
+const profile =
+  new Profile();
+
+
+// START SYSTEM
+
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+
+    console.log(
+      "SL V0.4 initialized"
+    );
+
+    setupInput(
+      memory,
+      mood,
+      profile
+    );
+  }
+);
